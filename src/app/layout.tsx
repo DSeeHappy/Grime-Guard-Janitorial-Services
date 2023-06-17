be3adfec28486a803f9da31e8083a2d8b5bc6b44
Daniel Sanchez <ds@dseehappy.com>
2023-06-17 17:57:10 -0600

    og opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Head from "next/head";
+import React from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +17,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <Head>
+      <title>
+        Grime Guard Janitorial Services LLC
+      </title>
+
+      <meta property="og:url" content="https://grimeguardjanitorialservices.com" />
+      <meta property="og:type" content="website" />
+      <meta
+          property="og:title"
+          content="Grime Guard Janitorial Services"
+      />
+      <meta name="twitter:card" content="Lakewood's Local Choice for Superior Janitorial Services Across Colorado's Vibrant Front Range Communities" />
+      <meta name="twitter:image" content="/Grime Guard Logo.png" />
+      <meta
+          property="og:description"
+          content="Lakewood's Local Choice for Superior Janitorial Services Across Colorado's Vibrant Front Range Communities"
+      />
+      <meta property="og:image" content="/Grime Guard Logo.png" />
+    </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
