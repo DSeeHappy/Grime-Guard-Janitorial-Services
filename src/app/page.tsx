5eaad0b934a485419544cd3960f0b15775078cf7
Daniel Sanchez <ds@dseehappy.com>
2023-06-16 23:00:40 -0600

    content updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ import {
     LockClosedIcon,
     ServerIcon,
 } from '@heroicons/react/20/solid'
+import Image from "next/image";
 
 const topNav = [
     { name: 'Services', href: '#services' },
@@ -211,12 +212,14 @@ export default function Example() {
                     <div className="mx-auto max-w-7xl">
                         <div className="px-6 pt-6 lg:max-w-2xl lg:pl-8 lg:pr-0">
                             <nav className="flex items-center justify-between lg:justify-start" aria-label="Global">
-                                <a href="#" className="-m-1.5 p-1.5">
-                                    <span className="sr-only">Your Company</span>
-                                    <img
-                                        alt="Your Company"
-                                        className="h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                <a href="/" className="-m-1.5 p-1.5">
+                                    <span className="sr-only">Grime Guard Janitorial Services</span>
+                                    <Image
+                                        alt="Grime Guard Janitorial Services Logo"
+                                        className="h-20 w-auto"
+                                        src="/Grime Guard Logo@3x.png"
+                                        height={250}
+                                        width={250}
                                     />
                                 </a>
                                 <button
@@ -242,11 +245,13 @@ export default function Example() {
                         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                             <div className="flex items-center justify-between">
                                 <a href="#" className="-m-1.5 p-1.5">
-                                    <span className="sr-only">Your Company</span>
-                                    <img
-                                        className="h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                                        alt=""
+                                    <span className="sr-only">Grime Guard Janitorial Services</span>
+                                    <Image
+                                        alt="Grime Guard Janitorial Services Logo"
+                                        className="h-25 w-auto"
+                                        src="/Grime Guard Logo@3x.png"
+                                        height={150}
+                                        width={150}
                                     />
                                 </a>
                                 <button
@@ -328,14 +333,15 @@ export default function Example() {
                         </div>
                     </div>
                     <div className="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-                        <img
+                        <Image
                             className="aspect-[3/2] object-cover lg:aspect-auto lg:h-full lg:w-full"
-                            src="https://images.unsplash.com/photo-1521737711867-e3b97375f902?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"
-                            alt=""
+                            src="/content/mainImage.jpg"
+                            alt="Image by macrovector on Freepik"
+                            height={1920}
+                            width={1080}
                         />
                     </div>
                 </div>
-
                 <div className="bg-white py-24 sm:py-32">
                     <div className="mx-auto max-w-7xl px-6 lg:px-8">
                         <div className="mx-auto max-w-2xl lg:mx-0">
@@ -387,10 +393,12 @@ export default function Example() {
                     <div className="mx-auto flex max-w-7xl flex-col items-center gap-x-8 gap-y-10 px-6 sm:gap-y-8 lg:px-8 xl:flex-row xl:items-stretch">
                         <div className="-mt-8 w-full max-w-2xl xl:-mb-8 xl:w-96 xl:flex-none">
                             <div className="relative aspect-[2/1] h-full md:-mx-8 xl:mx-0 xl:aspect-auto">
-                                <img
+                                <Image
                                     className="absolute inset-0 h-full w-full rounded-2xl bg-gray-800 object-cover shadow-2xl"
-                                    src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80"
-                                    alt=""
+                                    src="/content/cleaning-office-1.jpg"
+                                    alt="Image by Freepik"
+                                    height={250}
+                                    width={250}
                                 />
                             </div>
                         </div>
@@ -435,10 +443,12 @@ export default function Example() {
                                     </p>
                                 </div>
                                 <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg bg-gray-100">
-                                    <img
-                                        src="https://tailwindui.com/img/ecommerce-images/incentives-07-hero.jpg"
+                                    <Image
+                                        src="/content/cleaning-office-2.jpg"
                                         alt=""
                                         className="object-cover object-center"
+                                        height={250}
+                                        width={500}
                                     />
                                 </div>
                             </div>
@@ -481,35 +491,15 @@ export default function Example() {
                             </div>
                             <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
                                 <div className="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
-                                    <img
-                                        src="https://images.unsplash.com/photo-1670272502246-768d249768ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&q=80"
+                                    <Image
+                                        src="/content/team-image-2.jpg"
+                                        height={250}
+                                        width={250}
                                         alt=""
                                         className="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover"
                                     />
                                 </div>
-                                <div className="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
-                                    <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
-                                        <img
-                                            src="https://images.unsplash.com/photo-1605656816944-971cd5c1407f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80"
-                                            alt=""
-                                            className="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
-                                        />
-                                    </div>
-                                    <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
-                                        <img
-                                            src="https://images.unsplash.com/photo-1568992687947-868a62a9f521?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&h=842&q=80"
-                                            alt=""
-                                            className="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
-                                        />
-                                    </div>
-                                    <div className="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
-                                        <img
-                                            src="https://images.unsplash.com/photo-1612872087720-bb876e2e67d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80"
-                                            alt=""
-                                            className="aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover"
-                                        />
-                                    </div>
-                                </div>
+
                             </div>
                         </div>
                     </div>
@@ -520,10 +510,12 @@ export default function Example() {
                     <div className="relative isolate">
                         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                             <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
-                                <img
+                                <Image
+                                    src="/content/team-image-3.jpg"
+                                    height={250}
+                                    width={500}
+                                    alt="Image by wirestock on Freepik"
                                     className="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm"
-                                    src="https://images.unsplash.com/photo-1519338381761-c7523edc1f46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-                                    alt=""
                                 />
                                 <div className="w-full flex-auto">
                                     <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">The Grime Guard Difference</h2>
@@ -565,9 +557,11 @@ export default function Example() {
 
                 <div className="relative bg-gray-900">
                     <div className="relative h-80 overflow-hidden bg-indigo-600 md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2">
-                        <img
+                        <Image
                             className="h-full w-full object-cover"
-                            src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply"
+                            src="/content/cleaning-office-3.jpg"
+                            height={500}
+                            width={900}
                             alt=""
                         />
                         <svg
@@ -653,10 +647,12 @@ export default function Example() {
                             </div>
                         </div>
                         <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
-                            <img
+                            <Image
                                 className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
-                                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-                                alt=""
+                                src="/content/organization-clean-1.jpg"
+                                height={500}
+                                width={500}
+                                alt="Image by Freepik"
                             />
                         </div>
                         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
@@ -702,9 +698,11 @@ export default function Example() {
                         <div className="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                             <div className="lg:pr-4">
                                 <div className="relative overflow-hidden rounded-3xl bg-gray-900 px-6 pb-9 pt-64 shadow-2xl sm:px-12 lg:max-w-lg lg:px-8 lg:pb-8 xl:px-10 xl:pb-10">
-                                    <img
+                                    <Image
                                         className="absolute inset-0 h-full w-full object-cover brightness-125 saturate-0"
-                                        src="https://images.unsplash.com/photo-1630569267625-157f8f9d1a7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2669&q=80"
+                                        src="/content/spray-bottles-plain-background.jpg"
+                                        height={500}
+                                        width={500}
                                         alt=""
                                     />
                                     <div className="absolute inset-0 bg-gray-900 mix-blend-multiply" />
@@ -721,19 +719,7 @@ export default function Example() {
                                         />
                                     </div>
                                     <figure className="relative isolate">
-                                        <svg
-                                            viewBox="0 0 162 128"
-                                            fill="none"
-                                            aria-hidden="true"
-                                            className="absolute -left-2 -top-4 -z-10 h-32 stroke-white/20"
-                                        >
-                                            <path
-                                                id="0ef284b8-28c2-426e-9442-8655d393522e"
-                                                d="M65.5697 118.507L65.8918 118.89C68.9503 116.314 71.367 113.253 73.1386 109.71C74.9162 106.155 75.8027 102.28 75.8027 98.0919C75.8027 94.237 75.16 90.6155 73.8708 87.2314C72.5851 83.8565 70.8137 80.9533 68.553 78.5292C66.4529 76.1079 63.9476 74.2482 61.0407 72.9536C58.2795 71.4949 55.276 70.767 52.0386 70.767C48.9935 70.767 46.4686 71.1668 44.4872 71.9924L44.4799 71.9955L44.4726 71.9988C42.7101 72.7999 41.1035 73.6831 39.6544 74.6492C38.2407 75.5916 36.8279 76.455 35.4159 77.2394L35.4047 77.2457L35.3938 77.2525C34.2318 77.9787 32.6713 78.3634 30.6736 78.3634C29.0405 78.3634 27.5131 77.2868 26.1274 74.8257C24.7483 72.2185 24.0519 69.2166 24.0519 65.8071C24.0519 60.0311 25.3782 54.4081 28.0373 48.9335C30.703 43.4454 34.3114 38.345 38.8667 33.6325C43.5812 28.761 49.0045 24.5159 55.1389 20.8979C60.1667 18.0071 65.4966 15.6179 71.1291 13.7305C73.8626 12.8145 75.8027 10.2968 75.8027 7.38572C75.8027 3.6497 72.6341 0.62247 68.8814 1.1527C61.1635 2.2432 53.7398 4.41426 46.6119 7.66522C37.5369 11.6459 29.5729 17.0612 22.7236 23.9105C16.0322 30.6019 10.618 38.4859 6.47981 47.558L6.47976 47.558L6.47682 47.5647C2.4901 56.6544 0.5 66.6148 0.5 77.4391C0.5 84.2996 1.61702 90.7679 3.85425 96.8404L3.8558 96.8445C6.08991 102.749 9.12394 108.02 12.959 112.654L12.959 112.654L12.9646 112.661C16.8027 117.138 21.2829 120.739 26.4034 123.459L26.4033 123.459L26.4144 123.465C31.5505 126.033 37.0873 127.316 43.0178 127.316C47.5035 127.316 51.6783 126.595 55.5376 125.148L55.5376 125.148L55.5477 125.144C59.5516 123.542 63.0052 121.456 65.9019 118.881L65.5697 118.507Z"
-                                            />
-                                            <use href="#0ef284b8-28c2-426e-9442-8655d393522e" x={86} />
-                                        </svg>
-                                        <img src="https://tailwindui.com/img/logos/workcation-logo-white.svg" alt="" className="h-12 w-auto" />
+                                        <Image src="/Grime Guard Logo@3x.png" alt="Image by Freepik" className="h-12 w-auto" height={500} width={500} />
                                         <blockquote className="mt-6 text-xl font-semibold leading-8 text-white">
                                             <p>
                                                 Perfecting cleanliness with passion in Lakewood and across the vibrant neighboring communities of Colorado's Front Range.
@@ -741,8 +727,6 @@ export default function Example() {
                                         </blockquote>
                                         <figcaption className="mt-6 text-sm leading-6 text-gray-300">
                                             <strong className="font-semibold text-white">Locally Owned, Community Focused </strong> | Choose local. Choose Grime Guard.
-
-
                                         </figcaption>
                                     </figure>
                                 </div>
@@ -786,9 +770,11 @@ export default function Example() {
                     </div>
                 </div>
                 <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-                    <img
-                        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-                        alt=""
+                    <Image
+                        src="/content/cleaning-office-4.jpg"
+                        height={500}
+                        width={500}
+                        alt="Image by Freepik"
                         className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
                     />
                     <div className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl">
@@ -945,7 +931,12 @@ export default function Example() {
                                 </p>
                             </form>
                             <div className="lg:mt-6 lg:w-80 lg:flex-none">
-                                <img className="h-12 w-auto" src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg" alt="" />
+                                <Image
+                                    className="h-18 w-auto"
+                                    src="/Grime Guard Logo.png"
+                                    height={500}
+                                    width={500}
+                                    alt="" />
                                 <figure className="mt-10">
                                     <blockquote className="text-lg font-semibold leading-8 text-gray-900">
                                         <p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,9 @@ module.exports = {
       },
     },
   },
+  images: {
+    formats: ['image/avif', 'image/webp']
+  },
   plugins: [],
 }
 
